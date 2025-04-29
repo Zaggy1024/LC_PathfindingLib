d6d4972e3befe13e729e1160287b301b4d2af8c4
Zaggy1024 <Zaggy1024@gmail.com>
2025-04-29 17:13:48 -0500

    Add argument null checks in the SmartPathfinding registration methods

--- PathfindingLib/API/SmartPathfinding/SmartPathfinding.cs
+++ PathfindingLib/API/SmartPathfinding/SmartPathfinding.cs
@@ -1,4 +1,6 @@
-﻿using PathfindingLib.Data;
+using System;
+
+using PathfindingLib.Data;
 
 namespace PathfindingLib.API.SmartPathfinding;
 
@@ -8,11 +10,15 @@ public static class SmartPathfinding
 {
     public static void RegisterElevatorFloor(ElevatorFloor floor)
     {
+        if (floor == null)
+            throw new ArgumentNullException(nameof(floor));
         SmartPathLinks.RegisterElevatorFloor(floor);
     }
 
     public static void UnregisterElevatorFloor(ElevatorFloor floor)
     {
+        if (floor == null)
+            throw new ArgumentNullException(nameof(floor));
         SmartPathLinks.UnregisterElevatorFloor(floor);
     }
 }
